dbc7f3175d270bec09f9ba6de3f9d6e9140f603e
Thanh Tran <nick120187@gmail.com>
2017-10-11 19:06:53 -0700

    updates for vendor; added autoload psr-4

---
+++ src/classes/App.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Blog;
+
+class App {
+
+    public function __construct() {
+
+    }
+
+    public function loadRoutes() {
+
+    }
+
+    public function getRoutes() {
+
+    }
+}
---
+++ src/public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+// Load the autoload for vendor
+require_once __DIR__.'/../../vendor/autoload.php';
+
+$app = new \Blog\App();
+
+echo 'hello world';
