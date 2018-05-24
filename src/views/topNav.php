c6d1c892c0eec863fddb38fe8c529c3be05fce41
Thanh Tran <nick120187@gmail.com>
2018-05-23 22:47:18 -0700

    removed the top image in the header; updated the font-size too

--- src/views/topNav.php
+++ src/views/topNav.php
@@ -1,9 +1,6 @@
 <?php // Template for the top navigation ?>
 
 <div class="nav-header">
-    <div class="top-image">
-        <a href="/"><img src="//via.placeholder.com/180x220"></a>
-    </div>
     <div class="top-nav">
         <div class="nav-item" data-item="photography"><a href="/photography">photography</a></div>
         <div class="nav-item" data-item="web-stuff"><a href="<?= \Blog\View::BLOGS_PREFIX ?>/web">web stuff</a></div>
