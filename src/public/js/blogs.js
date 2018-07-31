1b308e1addab71a878c676b14f7b37cf5960ad7f
Thanh Tran <nick120187@gmail.com>
2018-07-30 22:45:52 -0700

    clicking on images in blogs will open a modal with the full/expanded resolution

---
+++ src/public/js/blogs.js
@@ -0,0 +1,31 @@
+(()=>{
+    class Blog {
+        constructor() {
+            this.expandedImageModal = $('.modal.view-expanded-image');
+        }
+
+        init() {
+            this.expandedImageModal.modal({
+                show: false,
+                keyboard: false
+            });
+            this.setListeners();
+        };
+
+        setListeners() {
+            let self = this;
+            $(document).on('click', '.single-blog img, .main-body-container img', function(e) {
+                e.preventDefault();
+                $('img', self.expandedImageModal).attr('src', '');
+
+                let src = $(this).attr('src').replace(/1000px|500px|250px/gi, 'full');
+
+                $('img', self.expandedImageModal).attr('src', src);
+                self.expandedImageModal.modal('show');
+            });
+        }
+    }
+
+    window.b = new Blog();
+    window.b.init();
+})();
