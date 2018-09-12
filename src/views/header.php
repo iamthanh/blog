30bfa0c3a7cb5289c96f6051d4838c5874d450d0
Thanh Tran <nick120187@gmail.com>
2018-09-12 02:07:14 -0700

    removed unneeded fonts; cleaned up the header.php a bit

--- src/views/header.php
+++ src/views/header.php
@@ -11,13 +11,18 @@
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
           integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans|Quicksand:300,400|Raleway" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400" rel="stylesheet">
 
     <link rel="stylesheet" href="/src/public/css/global.css">
 
     <script src="https://use.fontawesome.com/ea714fcd83.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
           integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+
+    <?= \Blog\GoogleAnalytics::getGATrackingScript(); ?>
+    <?= \Blog\Ads::getAdSenseHeadScript(); ?>
+</head>
+<body>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
@@ -29,8 +34,3 @@
     <link rel="stylesheet" href="/lib/highlight/agate.css">
     <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
     <script>hljs.initHighlightingOnLoad();</script>
-
-    <?= \Blog\GoogleAnalytics::getGATrackingScript(); ?>
-    <?= \Blog\Ads::getAdSenseHeadScript(); ?>
-</head>
-<body>
