2c1b9e7c8e87fc457a8ea730eaccd365399b200f
Thanh Tran <nick120187@gmail.com>
2018-10-14 02:17:13 -0700

    added bootstrap and jquery files to be used locally

--- src/views/header.php
+++ src/views/header.php
@@ -10,8 +10,7 @@
 
     <title><?= \Blog\View::$pageTitle ?></title>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="/lib/bootstrap/bootstrap.min.css">
 
     <link rel="stylesheet" href="/src/public/css/global.css">
 
@@ -22,11 +21,9 @@
     <?= \Blog\Ads::getAdSenseHeadScript(); ?>
 </head>
 <body>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-            integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-            integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="/lib/jQuery/jquery-3.3.1.min.js"></script>
+    <script src="/lib/bootstrap/popper.min.js"></script>
+    <script src="/lib/bootstrap/bootstrap.min.js"></script>
 
     <script src="/lib/timeago.min.js"></script>
     <script src="/lib/selectize/selectize.min.js"></script>
